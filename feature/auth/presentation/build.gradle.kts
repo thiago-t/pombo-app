05d2ea2a3000d88b4137c43674e9e0f91ce8d963
Thiago Toazza <thiagotoazza@outlook.com>
2026-03-24 16:14:16 -0300

    Defined Module Relations

--- feature/auth/presentation/build.gradle.kts
+++ feature/auth/presentation/build.gradle.kts
@@ -60,13 +60,11 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
-            }
-        }
 
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
+                implementation(projects.feature.auth.domain)
+                implementation(projects.core.domain)
+                implementation(projects.core.designsystem)
+                implementation(projects.core.presentation)
             }
         }
 
@@ -75,14 +73,6 @@ kotlin {
                 // Add Android-specific dependencies here. Note that this source set depends on
                 // commonMain by default and will correctly pull the Android artifacts of any KMP
                 // dependencies declared in commonMain.
-            }
-        }
-
-        getByName("androidDeviceTest") {
-            dependencies {
-                implementation(libs.androidx.runner)
-                implementation(libs.androidx.test.core)
-                implementation(libs.androidx.junit)
             }
         }
 
