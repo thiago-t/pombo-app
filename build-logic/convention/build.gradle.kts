26e5c32340abef00825c3a8faf85760684e1395c
Thiago Toazza <thiagotoazza@outlook.com>
2026-03-25 20:51:43 -0300

    Added Room Convention Plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.androidx.room.gradle.plugin)
     implementation(libs.buildkonfig.gradlePlugin)
     implementation(libs.buildkonfig.compiler)
 }
@@ -63,6 +64,10 @@ gradlePlugin {
         register("buildKonfig") {
             id = "com.ttlabz.convention.buildkonfig"
             implementationClass = "BuildKonfigConventionPlugin"
+        }
+        register("room") {
+            id = "com.ttlabz.convention.room"
+            implementationClass = "RoomConventionPlugin"
         }
     }
 }
