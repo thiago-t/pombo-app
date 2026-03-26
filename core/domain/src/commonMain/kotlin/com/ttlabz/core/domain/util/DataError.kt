4b5947a7db8a81e3ae6db215ecc8f910939f73e2
Thiago Toazza <thiagotoazza@outlook.com>
2026-03-25 22:04:19 -0300

    Added Result Wrapper utility function

---
+++ core/domain/src/commonMain/kotlin/com/ttlabz/core/domain/util/DataError.kt
@@ -0,0 +1,27 @@
+package com.ttlabz.core.domain.util
+
+sealed interface DataError : Error {
+
+    enum class Remote : DataError {
+        BAD_REQUEST,
+        REQUEST_TIMEOUT,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        CONFLICT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET_CONNECTION,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERVICE_UNAVAILABLE,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local : DataError {
+        DISK_FULL,
+        NOT_FOUND,
+        UNKNOWN
+    }
+
+}
---
+++ core/domain/src/commonMain/kotlin/com/ttlabz/core/domain/util/Error.kt
@@ -0,0 +1,3 @@
+package com.ttlabz.core.domain.util
+
+interface Error
