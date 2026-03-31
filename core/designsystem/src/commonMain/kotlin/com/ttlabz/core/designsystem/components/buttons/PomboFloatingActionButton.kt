d48d0413ad676279d8d6db702c0f995963218959
Thiago Toazza <thiagotoazza@outlook.com>
2026-03-30 23:25:48 -0300

    Added Floating Action Button Composable

---
+++ core/designsystem/src/commonMain/kotlin/com/ttlabz/core/designsystem/components/buttons/PomboFloatingActionButton.kt
@@ -0,0 +1,44 @@
+package com.ttlabz.core.designsystem.components.buttons
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ttlabz.core.designsystem.theme.PomboTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PomboFloatingActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        content = content
+    )
+}
+
+@Composable
+@Preview
+fun PomboFloatingActionButtonPreview() {
+    PomboTheme {
+        PomboFloatingActionButton(
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
+        }
+    }
+}
