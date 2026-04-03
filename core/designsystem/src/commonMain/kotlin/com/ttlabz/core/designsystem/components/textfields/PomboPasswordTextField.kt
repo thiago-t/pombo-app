5e6a0195757c656bac464606aac938faad457562
Thiago Toazza <thiagotoazza@outlook.com>
2026-04-02 21:30:26 -0300

    Added Password Text Fields

---
+++ core/designsystem/src/commonMain/kotlin/com/ttlabz/core/designsystem/components/textfields/PomboPasswordTextField.kt
@@ -0,0 +1,181 @@
+package com.ttlabz.core.designsystem.components.textfields
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicSecureTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.TextObfuscationMode
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.ttlabz.core.designsystem.theme.PomboTheme
+import com.ttlabz.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import pombo.core.designsystem.generated.resources.Res
+import pombo.core.designsystem.generated.resources.eye_icon
+import pombo.core.designsystem.generated.resources.eye_off_icon
+import pombo.core.designsystem.generated.resources.hide_password
+import pombo.core.designsystem.generated.resources.show_password
+
+@Composable
+fun PomboPasswordTextField(
+    state: TextFieldState,
+    isPasswordVisible: Boolean,
+    onToggleVisibilityClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    title: String? = null,
+    supportingText: String? = null,
+    isError: Boolean = false,
+    enabled: Boolean = true,
+    onFocusChanged: (Boolean) -> Unit = {},
+) {
+    PomboTextFieldLayout(
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
+        modifier = modifier
+    ) { styleModifier, interactionSource ->
+        BasicSecureTextField(
+            state = state,
+            modifier = styleModifier,
+            enabled = enabled,
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.onSurface
+                } else {
+                    MaterialTheme.colorScheme.extended.textPlaceholder
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            interactionSource = interactionSource,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
+            textObfuscationMode = if (isPasswordVisible) TextObfuscationMode.Visible else TextObfuscationMode.Hidden,
+            decorator = { innerBox ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        if (state.text.isEmpty() && placeholder != null) {
+                            Text(
+                                text = placeholder,
+                                color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                        innerBox()
+                    }
+
+                    Icon(
+                        imageVector = if (isPasswordVisible) {
+                            vectorResource(Res.drawable.eye_off_icon)
+                        } else {
+                            vectorResource(Res.drawable.eye_icon)
+                        },
+                        contentDescription = if (isPasswordVisible) {
+                            stringResource(Res.string.hide_password)
+                        } else {
+                            stringResource(Res.string.show_password)
+                        },
+                        tint = MaterialTheme.colorScheme.extended.textDisabled,
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = ripple(
+                                    bounded = false,
+                                    radius = 24.dp
+                                ),
+                                onClick = onToggleVisibilityClick
+                            )
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun PomboPasswordTextFieldEmptyPreview() {
+    PomboTheme {
+        PomboPasswordTextField(
+            state = rememberTextFieldState(),
+            isPasswordVisible = true,
+            onToggleVisibilityClick = {},
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "Password",
+            title = "Password",
+            supportingText = "Use at least 9+ characters...",
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun PomboPasswordTextFieldFilledPreview() {
+    PomboTheme {
+        PomboPasswordTextField(
+            state = rememberTextFieldState("password123"),
+            isPasswordVisible = false,
+            onToggleVisibilityClick = {},
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "Password",
+            title = "Password",
+            supportingText = "Use at least 9+ characters...",
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true
+)
+fun PomboPasswordTextFieldErrorPreview() {
+    PomboTheme {
+        PomboPasswordTextField(
+            state = rememberTextFieldState(),
+            isPasswordVisible = false,
+            onToggleVisibilityClick = {},
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "Password",
+            title = "Password",
+            supportingText = "Use at least 9+ characters...",
+            isError = true
+        )
+    }
+}
--- core/designsystem/src/commonMain/kotlin/com/ttlabz/core/designsystem/components/textfields/PomboTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/ttlabz/core/designsystem/components/textfields/PomboTextField.kt
@@ -1,17 +1,8 @@
 package com.ttlabz.core.designsystem.components.textfields
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -20,9 +11,6 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
@@ -45,27 +33,14 @@ fun PomboTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    val isFocused by interactionSource.collectIsFocusedAsState()
-
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-
-    Column(
-        modifier = modifier
-    ) {
-        if (title != null) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.extended.textSecondary
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-
+    PomboTextFieldLayout(
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
+        modifier = modifier,
+    ) { styleModifier, interactionSource ->
         BasicTextField(
             state = state,
             enabled = enabled,
@@ -86,29 +61,7 @@ fun PomboTextField(
             ),
             cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
             interactionSource = interactionSource,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = when {
-                        isFocused -> MaterialTheme.colorScheme.primary.copy(
-                            alpha = 0.5f
-                        )
-
-                        enabled -> MaterialTheme.colorScheme.surface
-                        else -> MaterialTheme.colorScheme.extended.secondaryFill
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .border(
-                    width = 1.dp,
-                    color = when {
-                        isError -> MaterialTheme.colorScheme.error
-                        isFocused -> MaterialTheme.colorScheme.primary
-                        else -> MaterialTheme.colorScheme.outline
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(12.dp),
+            modifier = styleModifier,
             decorator = { innerBox ->
                 Box(
                     modifier = Modifier
@@ -126,18 +79,6 @@ fun PomboTextField(
                 innerBox()
             }
         )
-        if (supportingText != null) {
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = supportingText,
-                color = if (isError) {
-                    MaterialTheme.colorScheme.error
-                } else {
-                    MaterialTheme.colorScheme.extended.textTertiary
-                },
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
     }
 }
 
