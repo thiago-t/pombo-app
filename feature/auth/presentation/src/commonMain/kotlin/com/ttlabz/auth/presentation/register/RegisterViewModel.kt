1f9f16078769eb5f1611435e3f9b584bc1def7fe
Thiago Toazza <thiagotoazza@outlook.com>
2026-04-07 19:36:39 -0300

    Validating Register Form Fields

--- feature/auth/presentation/src/commonMain/kotlin/com/ttlabz/auth/presentation/register/RegisterViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/ttlabz/auth/presentation/register/RegisterViewModel.kt
@@ -2,10 +2,18 @@ package com.ttlabz.auth.presentation.register
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ttlabz.auth.domain.EmailValidator
+import com.ttlabz.core.domain.validation.PasswordValidator
+import com.ttlabz.core.presentation.util.UiText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import pombo.feature.auth.presentation.generated.resources.Res
+import pombo.feature.auth.presentation.generated.resources.error_invalid_email
+import pombo.feature.auth.presentation.generated.resources.error_invalid_password
+import pombo.feature.auth.presentation.generated.resources.error_invalid_username
 
 class RegisterViewModel : ViewModel() {
 
@@ -27,8 +35,53 @@ class RegisterViewModel : ViewModel() {
 
     fun onAction(action: RegisterAction) {
         when (action) {
+            RegisterAction.OnLoginClick -> validateFormInputs()
             else -> {}
         }
+    }
+
+    private fun clearAllTextFieldErrors() {
+        _state.update {
+            it.copy(
+                emailError = null,
+                usernameError = null,
+                passwordError = null,
+                registrationError = null
+            )
+        }
+    }
+
+    private fun validateFormInputs(): Boolean {
+        clearAllTextFieldErrors()
+
+        val currentState = state.value
+        val email = currentState.emailTextState.text.toString()
+        val username = currentState.usernameTextState.text.toString()
+        val password = currentState.passwordTextState.text.toString()
+
+        val isEmailValid = EmailValidator.validate(email)
+        val passwordValidationState = PasswordValidator.validate(password)
+        val isUsernameValid = username.length in 3..20
+
+        val emailError = if (!isEmailValid) {
+            UiText.Resource(Res.string.error_invalid_email)
+        } else null
+        val usernameError = if (!isUsernameValid) {
+            UiText.Resource(Res.string.error_invalid_username)
+        } else null
+        val passwordError = if (!passwordValidationState.isValidPassword) {
+            UiText.Resource(Res.string.error_invalid_password)
+        } else null
+
+        _state.update {
+            it.copy(
+                emailError = emailError,
+                usernameError = usernameError,
+                passwordError = passwordError,
+            )
+        }
+
+        return isUsernameValid && isEmailValid && passwordValidationState.isValidPassword
     }
 
 }
