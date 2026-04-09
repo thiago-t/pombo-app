609b7e87ee06735992ac5fbc7270391ba3f9137c
Thiago Toazza <thiagotoazza@outlook.com>
2026-04-09 01:23:37 -0300

    Android Splash Screen Setup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,8 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
 
+            implementation(libs.core.splashscreen)
+
             implementation(libs.koin.android)
         }
         commonMain.dependencies {
